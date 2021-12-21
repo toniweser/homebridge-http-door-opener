8b10e7565cdb803fa9c31c1b31ba1c818bcb4a0e
Tobias Weber <tobias.weber@kurzdigital.com>
2021-12-21 23:49:16 +0100

    Implement states of lock accessory
    Please don't ask me 🤮

--- src/doorAccessory.ts
+++ src/doorAccessory.ts
@@ -38,6 +38,7 @@ export class DoorAccessory {
     // see https://developers.homebridge.io/#/service/Lightbulb
 
     // register handlers for the On/Off Characteristic
+
     this.service.getCharacteristic(this.platform.Characteristic.LockCurrentState)
       .onGet(this.handleLockCurrentStateGet.bind(this)); // GET - bind to the `getOn` method below;
 
@@ -50,43 +51,43 @@ export class DoorAccessory {
   /**
    * Handle requests to get the current value of the "Lock Current State" characteristic
    */
-  async handleLockCurrentStateGet(): Promise<CharacteristicValue> {
+  async handleLockCurrentStateGet() {
     this.platform.log.debug('Triggered GET LockCurrentState');
-
-    let currentValue;
-
-    if (this.doorStateLocked === 1) {
-      return this.platform.Characteristic.LockCurrentState.SECURED;
-    } else {
-      return this.platform.Characteristic.LockCurrentState.UNSECURED;
-    }
+    this.platform.log.debug(JSON.stringify(this.service.getCharacteristic(this.platform.Characteristic.LockCurrentState).value));
+    return 1;
   }
 
 
   /**
    * Handle requests to get the current value of the "Lock Target State" characteristic
    */
-  async handleLockTargetStateGet(): Promise<CharacteristicValue> {
+  async handleLockTargetStateGet() {
     this.platform.log.debug('Triggered GET LockTargetState');
+    this.platform.log.debug(JSON.stringify(this.service.getCharacteristic(this.platform.Characteristic.LockTargetState).value));
 
-    return this.doorStateLocked;
-
+    return 1;
   }
 
   /**
    * Handle requests to set the "Lock Target State" characteristic
+   * UNSECURED = 0;
+   * SECURED = 1;
    */
   async handleLockTargetStateSet(value) {
-    let valueToSet;
     if (value === 0) {
-      valueToSet = this.platform.Characteristic.LockTargetState.SECURED;
-      this.platform.log.debug('////// value === 0');
+      // Open door
+      this.service.getCharacteristic(this.platform.Characteristic.LockCurrentState).updateValue(0);
+      this.service.getCharacteristic(this.platform.Characteristic.LockTargetState).updateValue(1);
+
+      setTimeout(() => {
+        this.service.getCharacteristic(this.platform.Characteristic.LockCurrentState).updateValue(1);
+        this.service.getCharacteristic(this.platform.Characteristic.LockTargetState).updateValue(1);
+      }, 2000);
     } else {
-      valueToSet =this.platform.Characteristic.LockTargetState.SECURED;
-      this.platform.log.debug('////// value === 1');
+      // Close door
+      this.service.getCharacteristic(this.platform.Characteristic.LockCurrentState).updateValue(1);
+      this.service.getCharacteristic(this.platform.Characteristic.LockTargetState).updateValue(0);
     }
-    this.doorStateLocked = valueToSet;
-    this.platform.log.debug('Triggered SET LockTargetState:' + value);
   }
 
 }
